984af1a8628336c13f5d0e7f1b072b4f4a45342d
Avinash <bitsavinash@gmail.com>
2020-07-15 09:21:29 +0530

    Initial commit with hello

---
+++ src/main/java/tech/dipc/springbootdemo/HelloController.java
@@ -0,0 +1,15 @@
+package tech.dipc.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello!!!";
+    }
+
+
+}
---
+++ src/test/java/tech/dipc/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dipc.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
